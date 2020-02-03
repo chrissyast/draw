cf905335df88ba42e18f2f82fc7bab09a00c2921
chrissyast <chris.astles@indiecampers.com>
2020-02-03 14:20:03 +0000

    created new app --api --skip-active-record and integrated my new stuff

--- config/application.rb
+++ config/application.rb
@@ -3,17 +3,17 @@ require_relative 'boot'
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
-#require "active_job/railtie"
-#require "active_record/railtie"#
-require "active_storage/engine"
+require "active_job/railtie"
+# require "active_record/railtie"
+# require "active_storage/engine"
 require "action_controller/railtie"
-#require "action_mailer/railtie"
-#require "action_mailbox/engine"
-#require "action_text/engine"
-#require "action_view/railtie"
-#require "action_cable/engine"
+require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+require "action_view/railtie"
+require "action_cable/engine"
 # require "sprockets/railtie"
-#require "rails/test_unit/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -21,8 +21,6 @@ Bundler.require(*Rails.groups)
 
 module Draw
   class Application < Rails::Application
-
-  ENV.update YAML.load_file('config/application.yml')[Rails.env] rescue {}
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
