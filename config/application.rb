6de20b29e76d62129d2da1d9b967c095dd26c066
chrissyast <chris.astles@indiecampers.com>
2020-02-03 10:00:51 +0000

    create new api project and patch changes from original project onto this

--- config/application.rb
+++ config/application.rb
@@ -4,12 +4,12 @@ require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
-#require "active_record/railtie"
-#require "active_storage/engine"
+require "active_record/railtie"
+require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_mailbox/engine"
-#require "action_text/engine"
+require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
 # require "sprockets/railtie"
@@ -21,9 +21,11 @@ Bundler.require(*Rails.groups)
 
 module Draw
   class Application < Rails::Application
+
+  ENV.update YAML.load_file('config/application.yml')[Rails.env] rescue {}
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    ENV.update YAML.load_file('config/application.yml')[Rails.env] rescue {}
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
