20bf1f238ee6b93c995bd20422a2c5e58339db31
chrissyast <cjastles@gmail.com>
2020-11-08 16:05:42 +0000

    refactor animated part into separate component to share between main (new draw) and replay (old draw)

--- front/src/pages/main.vue
+++ front/src/pages/main.vue
@@ -1,34 +1,25 @@
 <template>
   <div class="main container">
-    <InputSection ref="input"
-                  :names="names"
-                  :showValidation="showValidation"
-                  :drawInProgress="drawInProgress"
-                  v-if="!(drawInProgress || drawFinished)"
-                  v-on:add="add"
-                  v-on:calculate="calculate"
-                  v-on:cancel="cancel"
+    <InputSection
+        ref="input"
+        :names="names"
+        :showValidation="showValidation"
+        :drawInProgress="drawInProgress"
+        v-if="!(drawInProgress || drawFinished)"
+        v-on:add="add"
+        v-on:calculate="calculate"
+        v-on:cancel="cancel"
     />
     <v-btn class="submit-button" style="width:25%" v-if="drawInProgress" @click="cancel">Cancel</v-btn>
-    <transition name="fade" mode="out-in">
-      <div v-if="!drawFinished">
-        <div
-            class="cardStack container"
-            style="height: 80vh; position: relative"
-        >
-          <CardStack
-              :gatherStatus="gatherStatus"
-              :names="names"
-              v-on:remove="remove"
-              ref="stack"
-          />
-        </div>
-      </div>
-      <result-section
-          v-if="drawFinished"
-          :results="drawResult">
-      </result-section>
-    </transition>
+    <animated-draw-section
+       ref="animation"
+       :names="names"
+       :draw-result="drawResult"
+       :drawId="drawId"
+       :drawFinished="drawFinished"
+       v-on:remove="remove"
+       v-on:drawOver="updateDrawFinished"
+    />
   </div>
 </template>
 
@@ -36,8 +27,8 @@
 
 import InputSection from "../components/InputSection.vue";
 import Card from "../components/Card.vue";
-import CardStack from "../components/CardStack.vue";
-import ResultSection from "../components/ResultSection";
+
+import AnimatedDrawSection from "@/components/AnimatedDrawSection";
 import API from "../api.js";
 
 export default {
@@ -45,17 +36,18 @@ export default {
   data: function () {
     return {
       names: [],
-      gatherStatus: "ungathered",
+   //   gatherStatus: "ungathered",
       showValidation: false,
       drawInProgress: false,
       drawFinished: false,
       displaySummary: false,
       timeouts: [],
       drawResult: {},
+      drawId: undefined,
     };
   },
-  refs: { stack: CardStack, input: InputSection },
-  components: {Card, CardStack, InputSection, ResultSection },
+  refs: { animation: AnimatedDrawSection, input: InputSection },
+  components: {Card, InputSection, AnimatedDrawSection },
   beforeCreate: function () {
     document.body.style = "overflow: visible";
     document.documentElement.style.setProperty(
@@ -66,26 +58,15 @@ export default {
   },
   methods: {
     toggleGather() {
-      this.drawInProgress = !this.drawInProgress;
-      if (this.gatherStatus === "ungathered") {
-        this.gatherStatus = "gathering";
-      } else {
-        this.gatherStatus = "ungathered";
-        this.$refs.stack.$refs.cards.forEach(c => c.selected = false)
-      }
+      this.$refs.animation.toggleGather()
     },
     animateDraw() {
-      var animationTime =
-          parseInt(this.$style.gatheringTime.split("ms")[0], 10) +
-          parseInt(this.$style.putAwayTime.split("ms")[0], 10);
-      this.timeouts.push(setTimeout(this.startDraw, animationTime));
+      this.$refs.animation.animateDraw()
     },
     cancel() {
+      this.drawInProgress = false;
       this.toggleGather();
-      for (var timeout in this.timeouts) {
-        clearTimeout(this.timeouts[timeout]);
-      }
-      this.$refs.stack.selectedBuyer = '';
+      this.$refs.animation.clearAnimations();
     },
     colour(index = 0) {
       if (!(Math.floor(index / 6) % 2))
@@ -93,11 +74,13 @@ export default {
       else return this.colours['--main-colour']
     },
     calculate() {
+      this.drawInProgress = true;
       this.toggleGather();
       const body = {"people": this.names};
       API.post("calculation", body)
           .then(response => {
             this.drawResult = response.data.result;
+            this.drawId = response.data.id
             this.showResults = true
             this.animateDraw();
           })
@@ -105,26 +88,9 @@ export default {
             this.errors.push(e)
           })
     },
-    startDraw() {
-      this.gatherStatus = "gathered";
-      var i = 1;
-      let delay = this.$style.nextCardDelay;
-      for (const [buyer, recipient] of Object.entries(this.drawResult)) {
-        let foo = i;
-
-        this.timeouts.push(
-            setTimeout(() => {
-              this.$refs.stack.selectCard(buyer, recipient, foo);
-            }, delay * i)
-        );
-        i++;
-      }
-      this.timeouts.push(
-          setTimeout(() => {
-            this.drawFinished = true;
-            this.drawInProgress = false;
-          }, delay * i + 1000)
-      );
+    updateDrawFinished() {
+      this.drawFinished = true;
+      this.drawInProgress = false;
     },
     add(event) {
       this.showValidation = false;
@@ -151,15 +117,7 @@ export default {
 };
 </script>
 
-<style module lang="scss">
-@import "src/styles/variables";
-:export {
-  maxUngathered: $maximum-ungathered-cards;
-  gatheringTime: $gathering-time;
-  putAwayTime: $put-away-time;
-  nextCardDelay: $next-card-delay;
-}
-</style>
+
 
 <style lang="scss" scoped>
 
@@ -167,11 +125,5 @@ export default {
   width: 100%;
   position: relative;
 }
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s;
-}
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-  opacity: 0;
-}
+
 </style>
--- front/src/pages/replay.vue
+++ front/src/pages/replay.vue
@@ -1,22 +1,35 @@
 <template>
 <div>
-  {{ result }}
+  <AnimatedDrawSection
+    ref="animate"
+    :draw-result="result"
+    :names="names"
+    gatherStatus="ungathered"/>
 </div>
 </template>
 
 <script>
 import api from "@/api";
+import AnimatedDrawSection from "@/components/AnimatedDrawSection";
 export default {
   name: "Replay",
+  refs: {animate: AnimatedDrawSection},
+  components: {AnimatedDrawSection},
   data: function() {
     return {
-    result: {}
+    result: {},
+    names: []
   }
   },
   created: function() {
     api.get(`results/${this.$route.params.id}`)
         .then(r => {
-          this.result = r.data;
+          this.result = r.data.result;
+          this.names = Object.keys(r.data.result)
+          setTimeout(() => {
+            this.$refs.animate.toggleGather()
+            this.$refs.animate.animateDraw()
+          }, 1000)
         })
         .catch(e => console.log(e))
   }
